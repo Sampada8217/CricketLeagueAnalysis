3046d84ce0b1308cdf2c93c6f4f42ad69fad94e6
sampada8217 <sampadatirthkar@gmail.com>
2020-05-22 17:01:41 +0530

    Refactored Code and Added new common function for get Sorted data to the list

--- src/main/java/com/CricketAnalyser/IPLAnalyser.java
+++ src/main/java/com/CricketAnalyser/IPLAnalyser.java
@@ -28,17 +28,86 @@ public class IPLAnalyser {
 
     }
 
-    public String getAverageWiseSortingOnData() throws IPLAnalyserException {
+    public  String  getSortedFunction(Comparator<IPLCSVDAO> iplcsvdaoComparator) throws IPLAnalyserException {
         if (iplDAOMap == null || iplDAOMap.size() == 0) {
             throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
-        Comparator<IPLCSVDAO> sortAvgIplComparator= Comparator.comparing(ipl -> ipl.avg);
         List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
                 .stream()
                 .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortAvgIplComparator);
+        this.sortByDescending(iplRunDAOS, iplcsvdaoComparator);
         String sortedAvgData = new Gson().toJson(iplRunDAOS);
         return sortedAvgData;
+    }
+
+    public String getAverageWiseSortingOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortAvgIplComparator= Comparator.comparing(ipl -> ipl.avg);
+        return getSortedFunction(sortAvgIplComparator);
+    }
+
+
+    public String getSRWiseSortingOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortSRIplComparator = Comparator.comparing(ipl -> ipl.sr);
+        return getSortedFunction(sortSRIplComparator);
+
+    }
+
+
+    public String get6sWiseSortingOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sort6sIplComparator = Comparator.comparing(ipl -> ipl.six);
+        return getSortedFunction(sort6sIplComparator);
+    }
+
+
+    public String get4sWiseSortingOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sort4sIplComparator = Comparator.comparing(ipl-> ipl.four);
+        return getSortedFunction(sort4sIplComparator);
+    }
+
+    public String getSRWiseSortingWith6sAnd4sOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
+        Comparator<IPLCSVDAO> sort6sComparator = sortSRComparator.thenComparing(census -> census.sr);
+        Comparator<IPLCSVDAO> sort4sCompartor = sort6sComparator.thenComparing(census -> census.four);
+        return getSortedFunction(sort4sCompartor);
+    }
+
+    public String getAverageWiseSortingWithSROnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortAvgComparator = Comparator.comparing(ipl -> ipl.avg);
+        Comparator<IPLCSVDAO> sortSRComparator = sortAvgComparator.thenComparing(census -> census.sr);
+        return getSortedFunction(sortSRComparator);
+    }
+
+    public String getRunsWiseSortingWithAvgOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortRunsComparator = Comparator.comparing(ipl -> ipl.runs);
+        Comparator<IPLCSVDAO> sortAvgComparator = sortRunsComparator.thenComparing(ipl -> ipl.avg);
+        return getSortedFunction(sortAvgComparator);
+    }
+
+    public String getBowlingEconomyRateWiseSortingOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortEconComparator = Comparator.comparing(ipl -> ipl.econ);
+        return getSortedFunction(sortEconComparator);
+    }
+    public String getBowlingSRWiseSortingWith5wAnd4wOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
+        Comparator<IPLCSVDAO> sort6sComparator = sortSRComparator.thenComparing(ipl -> ipl.fiveW);
+        Comparator<IPLCSVDAO> sort4sCompartor = sort6sComparator.thenComparing(ipl -> ipl.fourW);
+        return getSortedFunction(sort4sCompartor);
+    }
+    public String getWicketsWiseSortingWithAvgOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
+        Comparator<IPLCSVDAO> sort4wComparator = sortSRComparator.thenComparing(ipl -> ipl.fourW);
+        Comparator<IPLCSVDAO> sort5wComparator = sort4wComparator.thenComparing(ipl -> ipl.fiveW);
+        return getSortedFunction(sort5wComparator);
+    }
+    public String getSortingOnBattingAndBowlingAvgOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortBattingAvgComparator = Comparator.comparing(ipl -> ipl.avg);
+        Comparator<IPLCSVDAO> sortBowlingAvgComparator = sortBattingAvgComparator.thenComparing(ipl -> ipl.avg);
+        return getSortedFunction(sortBowlingAvgComparator);
+    }
+    public String getSortingOnMostRunsAndWicketsOnData() throws IPLAnalyserException {
+        Comparator<IPLCSVDAO> sortRunsComparator = Comparator.comparing(ipl -> ipl.runs);
+        Comparator<IPLCSVDAO> sortWktsComparator = sortRunsComparator.thenComparing(ipl -> ipl.wkts);
+        return getSortedFunction(sortWktsComparator);
     }
     private void sortByDescending(List<IPLCSVDAO> iplRunDAOS, Comparator<IPLCSVDAO> censusComparator) {
         for (int i = 0; i < iplRunDAOS.size() - 1; i++) {
@@ -52,164 +121,6 @@ public class IPLAnalyser {
             }
         }
     }
-
-    public String getSRWiseSortingOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortSRIplComparator = Comparator.comparing(ipl -> ipl.sr);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortSRIplComparator);
-        String sortedSRData = new Gson().toJson(iplRunDAOS);
-        return sortedSRData;
-
-    }
-
-
-    public String get6sWiseSortingOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sort6sIplComparator = Comparator.comparing(ipl -> ipl.six);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sort6sIplComparator);
-        String sorted6sData = new Gson().toJson(iplRunDAOS);
-        return sorted6sData;
-    }
-
-
-    public String get4sWiseSortingOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sort4sIplComparator = Comparator.comparing(ipl-> ipl.four);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sort4sIplComparator);
-        String sorted4sData = new Gson().toJson(iplRunDAOS);
-        return sorted4sData;
-    }
-
-    public String getSRWiseSortingWith6sAnd4sOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
-        Comparator<IPLCSVDAO> sort6sComparator = sortSRComparator.thenComparing(census -> census.sr);
-        Comparator<IPLCSVDAO> sort4sCompartor = sort6sComparator.thenComparing(census -> census.four);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sort4sCompartor);
-        String sorted4sData = new Gson().toJson(iplRunDAOS);
-        return sorted4sData;
-    }
-
-    public String getAverageWiseSortingWithSROnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortAvgComparator = Comparator.comparing(ipl -> ipl.avg);
-        Comparator<IPLCSVDAO> sortSRComparator = sortAvgComparator.thenComparing(census -> census.sr);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortSRComparator);
-        String sortedAvgData = new Gson().toJson(iplRunDAOS);
-        return sortedAvgData;
-
-    }
-
-    public String getRunsWiseSortingWithAvgOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortRunsComparator = Comparator.comparing(ipl -> ipl.runs);
-        Comparator<IPLCSVDAO> sortAvgComparator = sortRunsComparator.thenComparing(census -> census.avg);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortAvgComparator);
-        String sortedSRData = new Gson().toJson(iplRunDAOS);
-        return sortedSRData;
-    }
-
-    public String getBowlingEconomyRateWiseSortingOnData() throws IPLAnalyserException {
-
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortEconComparator = Comparator.comparing(ipl -> ipl.econ);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                                              .stream()
-                                              .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortEconComparator);
-        String sortedERData = new Gson().toJson(iplRunDAOS);
-        return sortedERData;
-    }
-    public String getBowlingSRWiseSortingWith5wAnd4wOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
-        Comparator<IPLCSVDAO> sort6sComparator = sortSRComparator.thenComparing(ipl -> ipl.fiveW);
-        Comparator<IPLCSVDAO> sort4sCompartor = sort6sComparator.thenComparing(ipl -> ipl.fourW);
-        List<IPLCSVDAO> iplWktsDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplWktsDAOS, sort4sCompartor);
-        String sorted4sData = new Gson().toJson(iplWktsDAOS);
-        return sorted4sData;
-    }
-    public String getWicketsWiseSortingWithAvgOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortSRComparator = Comparator.comparing(ipl -> ipl.sr);
-        Comparator<IPLCSVDAO> sort4wComparator = sortSRComparator.thenComparing(ipl -> ipl.fourW);
-        Comparator<IPLCSVDAO> sort5wComparator = sort4wComparator.thenComparing(ipl -> ipl.fiveW);
-
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sort5wComparator);
-        String sorted4sData = new Gson().toJson(iplRunDAOS);
-        return sorted4sData;
-
-    }
-    public String getSortingOnBattingAndBowlingAvgOnData() throws IPLAnalyserException {
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortBattingAvgComparator = Comparator.comparing(ipl -> ipl.avg);
-        Comparator<IPLCSVDAO> sortBowlingAvgComparator = sortBattingAvgComparator.thenComparing(ipl -> ipl.avg);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortBowlingAvgComparator);
-        String sortedAvgData = new Gson().toJson(iplRunDAOS);
-        return sortedAvgData;
-    }
-    public String getSortingOnMostRunsAndWicketsOnData() throws IPLAnalyserException {
-
-        if (iplDAOMap == null || iplDAOMap.size() == 0) {
-            throw new IPLAnalyserException("No Census Data", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        }
-        Comparator<IPLCSVDAO> sortRunsComparator = Comparator.comparing(ipl -> ipl.runs);
-        Comparator<IPLCSVDAO> sortWktsComparator = sortRunsComparator.thenComparing(ipl -> ipl.wkts);
-        List<IPLCSVDAO> iplRunDAOS = iplDAOMap.values()
-                .stream()
-                .collect(Collectors.toList());
-        this.sortByDescending(iplRunDAOS, sortWktsComparator);
-        String sortedData = new Gson().toJson(iplRunDAOS);
-        return sortedData;
-    }
-
 }
 
 
